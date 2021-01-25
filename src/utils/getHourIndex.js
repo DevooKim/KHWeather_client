3caed7702c0dbf7769cd637ec49b3d3802729ade
DevooKim <khwoo8026@gmail.com>
2021-01-26 03:03:19 +0900

    [fix] 현재 시간 인덱스 버그 수정

--- src/utils/getHourIndex.js
+++ src/utils/getHourIndex.js
@@ -6,10 +6,8 @@ export default function getHourIndex(hour, min = 0, offset = false) {
   const index = Math.ceil((hour % 24) / 3) % 8;
   const key = index * 3;
 
-  if (hour - key === 0) {
-    return index;
-  } else if (hour - key === 1 && min <= 30) {
-    return index;
+  if (key - hour === 2 && min <= 30) {
+    return index - 1;
   }
-  return index + 1;
+  return index;
 }
