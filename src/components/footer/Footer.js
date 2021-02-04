0d8c283527767701b6154b5462421f319cb52138
DevooKim <khwoo8026@gmail.com>
2021-02-04 09:32:18 +0900

    add OpenWeather Logo

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,7 +7,7 @@ const useStyles = makeStyles((theme) => ({
   footer: {
     display: "flex",
     alignItems: "center",
-    justifyContent: "flex-end",
+    justifyContent: "space-between",
     position: "fixed",
     bottom: 0,
     width: "100%",
@@ -15,6 +15,27 @@ const useStyles = makeStyles((theme) => ({
     background: fade(theme.colors.footer.bg, 0.8),
     paddingRight: theme.spacing(3),
     fontWeight: 700,
+    [theme.breakpoints.up("sm")]: {
+      justifyContent: "flex-end",
+    },
+  },
+  info: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: "0.8rem",
+  },
+  img: {
+    height: "3.5rem",
+    marginLeft: "-1rem",
+    marginRight: "-1rem",
+    [theme.breakpoints.up("sm")]: {},
+  },
+  desktopMode: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
   },
 }));
 
@@ -28,10 +49,21 @@ function Footer() {
   return (
     <>
       <div className={classes.footer}>
-        <p>개발자: DevooKim</p>
-        <IconButton onClick={() => gitClick()}>
-          <GitHubIcon />
-        </IconButton>
+        <div className={classes.info}>
+          <p>개발자: DevooKim</p>
+          <IconButton onClick={() => gitClick()}>
+            <GitHubIcon />
+          </IconButton>
+        </div>
+        <div className={classes.info}>
+          <p className={classes.desktopMode}>날씨제공</p>
+          <IconButton onClick={() => console.log("hi")}>
+            <img
+              className={classes.img}
+              src="https://user-images.githubusercontent.com/42219589/106826161-b1a87900-66c9-11eb-861b-c15ea17afc33.png"
+            />
+          </IconButton>
+        </div>
       </div>
     </>
   );
