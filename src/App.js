50e2056e53289eb72ab42b1bdf78c34e3fc12400
Kim Hyun Woo <42219589+DevooKim@users.noreply.github.com>
2022-01-08 06:56:03 +0900

    refactor: makeStyle 제거

--- src/App.js
+++ src/App.js
@@ -1,79 +1,82 @@
-import React, { createContext, useCallback, useMemo, useState } from "react";
+import React, { createContext, useMemo, useState } from "react";
 import Header from "./components/header/Header";
 import Daily from "./components/daily/Daily";
 import Forecast from "./components/forecast/Forecast";
 import Footer from "./components/footer/Footer";
 import WeatherData from "./components/weathers/WeatherData";
 
-import { Box, Container, IconButton, CssBaseline } from "@material-ui/core";
-import { Brightness7, Brightness4 } from "@material-ui/icons";
-// import { createMuiTheme, ThemeProvider, makeStyles } from "@material-ui/core/styles";
+import Brightness7 from "@mui/icons-material/Brightness7";
+import Brightness4 from "@mui/icons-material/Brightness4";
+import { Box, Container, IconButton, CssBaseline } from "@mui/material";
 import { themeLight, themeDark, chartLight, chartDark } from "./theme/theme.js";
 import "./theme/App.css";
 
-import { createTheme, ThemeProvider, adaptV4Theme } from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-// const useStyles = makeStyles((theme) => ({
-//   address: {
-//     display: "flex",
-//     alignItems: "center",
-//     marginTop: theme.spacing(1),
-//     justifyContent: "center",
-//     [theme.breakpoints.up("sm")]: {
-//       justifyContent: "space-between",
-//     },
-//   },
-// }));
+export const ColorModeContext = createContext();
+export const ChartTheme = createContext(chartLight);
 
 const initialState = {
   region: "대전광역시 서구 둔산동",
   geo: { lat: 36.354687, lon: 127.420997 },
 };
 
+const getDesignTokens = (mode) => (mode === "light" ? themeLight : themeDark);
+const getChartTheme = (mode) => (mode === "light" ? chartLight : chartDark);
+
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [mode, setMode] = useState("light");
   const [state, setState] = useState(initialState);
-  // const classes = useStyles();
 
-  const theme = useMemo(() => createTheme(adaptV4Theme(darkMode ? { themeDark } : { themeLight })), [darkMode]);
+  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+  const chartTheme = useMemo(() => getChartTheme(mode), [mode]);
 
-  const chartTheme = useMemo(() => {
-    return darkMode ? chartDark : chartLight;
-  }, [darkMode]);
-
-  const changeDarkMode = useCallback(() => {
-    setDarkMode((prev) => !prev);
-  }, []);
+  const colorMode = useMemo(
+    () => ({
+      // The dark mode switch would invoke this method
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
 
   const handleState = (value) => {
     return setState(value);
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Header setState={handleState}>
-        <IconButton onClick={() => changeDarkMode()} color="inherit">
-          {darkMode ? <Brightness7 /> : <Brightness4 />}
-        </IconButton>
-      </Header>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header setState={handleState}>
+          <IconButton onClick={() => colorMode.toggleColorMode()} color="inherit">
+            {mode === "light" ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+        </Header>
 
-      <Container maxWidth={"md"}>
-        <WeatherData geo={state.geo}>
-          {/* <Box className={classes.address} borderBottom={1}> */}
-          <Box borderBottom={1}>
-            <p>{state.region}</p>
-          </Box>
-          <ChartTheme.Provider value={chartTheme}>
-            <Forecast />
-          </ChartTheme.Provider>
-          <Daily />
-        </WeatherData>
-      </Container>
+        <Container maxWidth={"md"}>
+          <WeatherData geo={state.geo}>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: { xs: "center", sm: "space-between" },
+                mt: 1,
+              }}
+            >
+              {state.region}
+            </Box>
+            <ChartTheme.Provider value={chartTheme}>
+              <Forecast />
+            </ChartTheme.Provider>
+            <Daily />
+          </WeatherData>
+        </Container>
 
-      <Footer />
-    </ThemeProvider>
+        <Footer />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
-export const ChartTheme = createContext(chartLight);
 export default App;
