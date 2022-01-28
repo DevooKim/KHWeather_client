044356cf743bc62085af23690f6f48074238f5dd
Kim Hyun Woo <42219589+DevooKim@users.noreply.github.com>
2022-01-29 00:51:46 +0900

    [feature] eslint / prettier

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,45 +1,51 @@
-import React, { useState, useCallback } from "react";
-import { Paper, Box, Typography } from "@mui/material";
-import { IconButton } from "@material-ui/core";
-import Brightness7 from "@mui/icons-material/Brightness7";
-import Brightness4 from "@mui/icons-material/Brightness4";
-import AutocompleteSearchInput from "../../components/AutocompleteSearchInput";
+import React, { useState, useCallback } from 'react';
+import { Paper, Box, Typography } from '@mui/material';
+import { IconButton } from '@material-ui/core';
+import Brightness7 from '@mui/icons-material/Brightness7';
+import Brightness4 from '@mui/icons-material/Brightness4';
+import AutocompleteSearchInput from '../../components/AutocompleteSearchInput';
 
 const dummy = [1, 2, 3, 4, 5];
 const NewHeader = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [value, setValue] = useState("");
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [value, setValue] = useState('');
 
-  const onClick = useCallback(() => setIsDarkMode((prev) => !prev), []);
-  const onChange = (e) => {
-    setValue(e.target.value);
-  };
+    const onClick = useCallback(() => setIsDarkMode((prev) => !prev), []);
+    const onChange = (e) => {
+        setValue(e.target.value);
+    };
 
-  const onKeyDown = () => {
-    if (e.key !== "Enter") return;
-    console.log(value);
-    //change query
-  };
-  return (
-    <Paper
-      elevation={3}
-      sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", px: "4rem", py: "0.25rem" }}
-    >
-      <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-        <Typography variant="h4">KHWeather</Typography>
-        <IconButton onClick={onClick}>{isDarkMode ? <Brightness4 /> : <Brightness7 />}</IconButton>
-      </Box>
-      <AutocompleteSearchInput
-        label="지역 검색"
-        options={dummy}
-        size="small"
-        onChange={onChange}
-        onKeyDown={onKeyDown}
-        fullWidth={true}
-        sx={{ width: "15rem" }}
-      />
-    </Paper>
-  );
+    const onKeyDown = (e) => {
+        if (e.key !== 'Enter') return;
+        console.log(value);
+        // change query
+    };
+    return (
+        <Paper
+            elevation={3}
+            sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                px: '4rem',
+                py: '0.25rem'
+            }}
+        >
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                <Typography variant="h4">KHWeather</Typography>
+                <IconButton onClick={onClick}>{isDarkMode ? <Brightness4 /> : <Brightness7 />}</IconButton>
+            </Box>
+            <AutocompleteSearchInput
+                label="지역 검색"
+                options={dummy}
+                size="small"
+                onChange={onChange}
+                onKeyDown={onKeyDown}
+                fullWidth
+                sx={{ width: '15rem' }}
+            />
+        </Paper>
+    );
 };
 
 export default NewHeader;
--- src/utils/geoCoder.js
+++ src/utils/geoCoder.js
@@ -1,21 +1,21 @@
-import axios from "axios";
+import axios from 'axios';
 
 const headers = {
-  Authorization: `KakaoAK ${import.meta.env.VITE_KAKAO_API_KEY}`,
+	Authorization: `KakaoAK ${import.meta.env.VITE_KAKAO_API_KEY}`,
 };
 
 export const AddressSearch = async (address, setAddress) => {
-  const url = `https://dapi.kakao.com/v2/local/search/address.json?query=${address}`;
+	const url = `https://dapi.kakao.com/v2/local/search/address.json?query=${address}`;
 
-  try {
-    const response = await axios.get(url, { headers });
-    const data = response.data.documents;
-    if (data.length === 0) {
-      setAddress([{ address_name: "검색 결과가 없습니다." }]);
-    } else {
-      setAddress([...response.data.documents]);
-    }
-  } catch (e) {
-    console.log(e);
-  }
+	try {
+		const response = await axios.get(url, { headers });
+		const data = response.data.documents;
+		if (data.length === 0) {
+			setAddress([{ address_name: '검색 결과가 없습니다.' }]);
+		} else {
+			setAddress([...response.data.documents]);
+		}
+	} catch (e) {
+		console.log(e);
+	}
 };
